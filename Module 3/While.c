1c986f4c37a6a5c7d26e9b4883d765306b1ee1a7
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-04-05 22:41:33 +0600

    module 3

---
+++ Module 3/While.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    // for (i = 1; i <= 15; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // while
+    // i = 1;
+    // while (i <= 5)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+
+    // do while
+    i = 1;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 5);
+    return 0;
+}
