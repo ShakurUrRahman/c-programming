0dd754c5a156076db6a15b20c233f01c3986303f
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-04-11 21:27:45 +0600

    module 6

---
+++ Module 6/quiz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         printf("%d ", i);
+    //     }
+    // }
+
+    // return 0;
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         printf("%d ", i);
+    //     }
+    // }
+    int i = 10;
+    while (i >= 0)
+    {
+        printf("%d ", i);
+        i -= 2;
+    }
+}
